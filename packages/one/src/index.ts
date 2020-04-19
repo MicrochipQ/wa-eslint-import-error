85b0776f3890934181d88cccae073e4284241b98
MicrochipQ <microchipq@gmail.com>
2020-04-19 05:09:31 +0300

    feat: prepare to the new issue

--- packages/one/src/index.ts
+++ packages/one/src/index.ts
@@ -2,3 +2,6 @@ import somemodule from '~/somemodule';
 
 console.log('hello world');
 console.log(somemodule);
+
+const test_camel_case = true;
+console.log(test_camel_case);
--- packages/two/src/index.ts
+++ packages/two/src/index.ts
@@ -2,3 +2,6 @@ import module2 from '~/module';
 
 console.log('hello world');
 console.log(module2);
+
+const test_camel_case = true;
+console.log(test_camel_case);
